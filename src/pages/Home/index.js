464cd4dd3a51dfc527f33dec43db7e7cc2c08a5d
XBinhs17 <nguyenbinh17062003@gmai.com>
2024-08-21 22:18:08 +0700

    fix intern page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,29 +1,13 @@
 import QuestionsList from "~/components/QuestionForm/QuestionsList";
 import classNames from "classnames/bind";
 import styles from './Home.module.scss'
-import { useState } from "react";
 
 const cx = classNames.bind(styles);
 
 function Home() {
-
-   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-
-   const handleAdd = () => {
-       setIsAddModalOpen(true);
-   };
-
-   const handleCloseAddModal = () => {
-       setIsAddModalOpen(false);
-   };
-
     return (
        <div className={cx('wrapper')}>
-           <QuestionsList 
-                isAddModalOpen={isAddModalOpen} 
-                setIsAddModalOpen={setIsAddModalOpen} 
-                onAdd={handleAdd}
-            />
+          <QuestionsList/>
        </div>
     );
 }
